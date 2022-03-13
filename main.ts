e81dbb3718d816db2495b40aab0daa17f44920a3
jordicollell <78138546+jordicollell@users.noreply.github.com>
2022-03-13 09:58:24 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,17 @@
+let strip: neopixel.Strip = null
+basic.forever(function () {
+    // Create a NeoPixel driver - specify the pin, number of LEDs, and the type of
+    // the NeoPixel srip, either standard RGB (with GRB or RGB format) or RGB+White.
+    strip = neopixel.create(DigitalPin.P0, 24, NeoPixelMode.RGB)
+    // set pixel colors
+    // white
+    strip.setPixelColor(0, NeoPixelColors.White)
+    // red
+    strip.setPixelColor(1, 16711680)
+    // green
+    strip.setPixelColor(2, 65280)
+    // blue
+    strip.setPixelColor(3, NeoPixelColors.Blue)
+    // send the data to the strip
+    strip.show()
+})
